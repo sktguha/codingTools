647e5d8c6af9f435a7697e0d3576558a0c77e5a1
Saikat Guha <sktguha@gmail.com>
2018-05-31 22:47:41 +0530

    Initial commit

---
+++ fileSearcherHelper.js
@@ -0,0 +1,39 @@
+var ioHook = require('iohook');
+var ks = require('node-key-sender');
+var nrc = require('node-run-cmd');
+var robotjs = require('robotjs')
+var clipboardy = require('clipboardy');
+var path = require("path");
+
+function isFindUsagesEvent(e) {
+    return e.ctrlKey && e.keychar === 117;
+}
+
+ioHook.on("keypress", function(e){
+    try {
+        if (isFindUsagesEvent(e)) {
+            //ks.sendCombination(['control', 'shift', 'v']);
+            // nrc.run("java -jar 'C:/projects/codingTools/node_modules/node-key-sender/jar/key-sender.jar' control-shift-v")
+            //     .then(function(exitCodes) {
+            //         console.log("exit codes are ", exitCodes);
+            //     });
+            console.log("detect ctrl u");
+            setTimeout(function () {
+                robotjs.keyTap("c", ["control", "shift"]);
+                var filePath = clipboardy.readSync();
+                var name = filePath.split(path.sep).pop();
+                name = name.split(".")[0];
+                console.log("got path ", name);
+                robotjs.keyTap("f", ["control", "shift"]);
+                name.split("").forEach(function(key){
+                    robotjs.keyTap(key);
+                } );
+                console.log("sent search command for ", name);
+            }, 1000);
+        }
+    } catch (e) {
+        console.error("error occured", e.toString());
+    }
+});
+
+ioHook.start();
